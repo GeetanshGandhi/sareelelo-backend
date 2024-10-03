6134f23792761e10f79d866ca7f309c45790ae55
Akshx09 <135423776+Akshx09@users.noreply.github.com>
2024-10-03 23:44:19 +0530

    Update DeliveryEmployeeRepository.java

--- src/main/java/com/vastrika/backend/DeliveryEmployee/repository/DeliveryEmployeeRepository.java
+++ src/main/java/com/vastrika/backend/DeliveryEmployee/repository/DeliveryEmployeeRepository.java
@@ -1,9 +1,10 @@
 package com.vastrika.backend.DeliveryEmployee.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.vastrika.backend.DeliveryEmployee.model.DeliveryEmployee;
 
-import com.vastrika.backend.DeliveryEmployee.controller.DeliveryEmployeeController;
+@Repository
 
-import jakarta.persistence.Id;
-
-public class DeliveryEmployeeRepository {
-   
+public interface DeliveryEmployeeRepository extends JpaRepository < DeliveryEmployee,String>{
+   //repo
 }
